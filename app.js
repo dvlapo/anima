248ddc3bcb1daf719deed5a5fc5271abfc205641
Dolapo Agboola <agbooladolapo24@gmail.com>
2023-11-23 17:23:39 +0100

    :p

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ function scaleDownTittle() {
     const top = footer.getBoundingClientRect().top;
     const percentReveal = (top / window.innerHeight) * 100;
 
-    if (percentReveal <= 40) {
+    if (percentReveal <= 30) {
         tittle.classList.add("scaledown");
         setTimeout(() => {
             tittle.classList.add("bounce");
@@ -15,5 +15,20 @@ function scaleDownTittle() {
         tittle.classList.remove("bounce");
     }
 }
+
+function formatMinute(min) {
+    return min.toString().length < 2 ? `0${min}` : min;
+}
+
+function getTime() {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+
+    document.querySelector("#hour").innerHTML = hours;
+    document.querySelector("#minute").innerHTML = formatMinute(minutes);
+}
+getTime();
+setInterval(getTime, 60000);
 
 window.addEventListener("scroll", scaleDownTittle);
